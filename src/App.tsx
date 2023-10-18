664d6d877ebc90233046335534e21142fbe0faab
ThunderBoltCreator <lololol2763@gmail.com>
2023-10-18 22:11:38 +0300

    asdsad

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,42 @@
 import React from 'react'
 import {Route, Switch} from 'react-router-dom'
-import {IState} from './app/redux/store.types'
 
 import s from './assets/styles/appStyle.styled'
 import {Header} from './components/layout/header/Header'
 import {Sidebar} from './components/layout/sidebar/Sidebar'
 import {Dialogs} from './pages/dialogs/Dialogs'
 import {Profile} from './pages/profile/Profile'
-import {Checkbox} from './shared/checbox'
+import {IState} from './app/redux/state.types'
+import {Checkbox} from "./shared/checbox";
 
 interface IApp {
-  state: IState
-  addPost: (message: string) => void
-  changeNewPostText: (newText: string) => void
+   state: IState
+   addPost: (message: string) => void
 }
 
 function App(props: IApp) {
-  const {
-    state,
-    changeNewPostText,
-    addPost
-  } = props
+   const {
+      state,
+      addPost
+   } = props
 
-  return (
-    <s.AppWrapper>
-      <Header/>
-      <Sidebar/>
+   const 
 
-      <Checkbox/>
+   return (
+      <s.AppWrapper>
+         <Header/>
+         <Sidebar/>
 
-      <s.AppContent>
-        <Switch>
-          <Route path={'/profile'}
-                 render={() => <Profile
-                   profileState={state.profilePage}
-                   changeNewPostText={changeNewPostText}
-                   addPost={addPost}
-                 />}/>
-          <Route path={'/dialogs'}
-                 render={() => <Dialogs
-                   dialogsState={state.dialogsPage}
-                 />}/>
-        </Switch>
-      </s.AppContent>
-    </s.AppWrapper>
-  )
+         <Checkbox/>
+
+         <s.AppContent>
+            <Switch>
+               <Route path={'/profile'} render={() => <Profile profileState={state.profilePage} addPost={addPost} />}/>
+               <Route path={'/dialogs'} render={() => <Dialogs dialogsState={state.dialogsPage}/>}/>
+            </Switch>
+         </s.AppContent>
+      </s.AppWrapper>
+   )
 }
 
 export default App
